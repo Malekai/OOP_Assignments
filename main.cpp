48bb471a7f0f88961a5dc6dd441fbe5e04c8901c
Malek <mmalek1421@gmail.com>
2017-03-04 21:08:15 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Assignment Zero
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float n;
+    float area;
+    
+    cout << "Minhajul Malek" << endl;
+    cout << 0 << endl;
+    
+    cout << "Give me the radius of the circle " << endl;
+    cin >> n;
+    
+    area = 3.14159 * (n * n);
+    
+    cout << "The area is " << area << endl;
+    return 0;
+}
